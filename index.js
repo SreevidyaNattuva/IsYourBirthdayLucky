bcd657e7862342682c110efe2a8a38b49e5642b8
SreevidyaNattuva <75021824+SreevidyaNattuva@users.noreply.github.com>
2021-12-02 16:40:23 +0530

    FinalCommit

--- index.js
+++ index.js
@@ -1,9 +1,37 @@
 const bdayDate = document.querySelector("#dob");
 const luckyNumber = document.querySelector("#lucky-number");
 const checkButton = document.querySelector("#check-number");
-console.log(bdayDate.value);
-// checkButton.addEventListener('click',function checkBirthDayIsLucky(){
-//     var dobEntered = bdayDate.value;
+const outputShown = document.querySelector("#output");
+//console.log("hi");
 
-// })
-
+checkButton.addEventListener('click',checkBirthDayIsLucky)
+function checkBirthDayIsLucky(){
+    var dobEntered = bdayDate.value;
+    var luckynoEntered = luckyNumber.value;
+    //console.log(bdayDate.value);
+    var sum = calculateSum(dobEntered);
+    if(sum&&luckynoEntered){
+        if((sum%luckynoEntered)===0 )
+        {
+            console.log("lucky");
+            outputShown.innerText = "Hurray!!your birthday lucky!!";
+        }
+        else{
+        outputShown.innerText = "Not so lucky!! No problem buddy you make your own luck!!";
+        }
+    }   
+    else{
+      outputShown.innerText = "Please enter valid inputs";
+        }                                                                                                                 
+    
+}
+    
+function calculateSum(dobEntered){
+dobEntered = dobEntered.replaceAll("-","");
+let sum=0;
+for(let i=0; i<dobEntered.length;i++)
+{
+    sum = sum+Number(dobEntered.charAt(i));
+}
+return sum;
+}
