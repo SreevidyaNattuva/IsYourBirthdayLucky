9d3fba2a82d50306b20be569c68f0753db734c62
SreevidyaNattuva <75021824+SreevidyaNattuva@users.noreply.github.com>
2021-12-02 13:35:26 +0530

    cashRegisterInitialCommit
    cashRegisterInitialCommit

---
+++ index.js
@@ -0,0 +1,8 @@
+const dob = document.querySelector("#dob");
+const luckyNumber = document.querySelector("lucky-number");
+const checkButton = document.querySelector("#check-number");
+
+checkButton.addEventListener("click",function addNumbersofDOB(){
+    var dobEntered = dob.value;
+    console.log(dob);
+})
