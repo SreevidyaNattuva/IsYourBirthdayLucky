8432e6ce06757d9fab6b863821289935ce078168
SreevidyaNattuva <75021824+SreevidyaNattuva@users.noreply.github.com>
2021-12-02 14:12:45 +0530

    intermediatecommitBdayLucky
    intermediatecommitBdayLucky

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-const dob = document.querySelector("#dob");
-const luckyNumber = document.querySelector("lucky-number");
+const bdayDate = document.querySelector("#dob");
+const luckyNumber = document.querySelector("#lucky-number");
 const checkButton = document.querySelector("#check-number");
+console.log(bdayDate.value);
+// checkButton.addEventListener('click',function checkBirthDayIsLucky(){
+//     var dobEntered = bdayDate.value;
 
-checkButton.addEventListener("click",function addNumbersofDOB(){
-    var dobEntered = dob.value;
-    console.log(dob);
-})
+// })
+
